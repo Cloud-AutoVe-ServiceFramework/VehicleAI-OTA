69a3242640329ed2769858d3b9a87bdee586840b
jeow7 <49180544+jeow7@users.noreply.github.com>
2022-11-24 13:21:46 +0900

    Update a file

--- src/main/java/kr/re/etri/advcloud/ApplicationLoadHandler.java
+++ src/main/java/kr/re/etri/advcloud/ApplicationLoadHandler.java
@@ -2,22 +2,30 @@ package kr.re.etri.advcloud;
 
 import javax.annotation.ManagedBean;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 import kr.re.etri.advcloud.common.constant.FtpClientConstant;
-import kr.re.etri.advcloud.common.util.FtpClientUtils;
 
 @ManagedBean
-public class ApplicationLoadHandler implements ApplicationRunner  {
+public class ApplicationLoadHandler implements ApplicationRunner {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ApplicationLoadHandler.class);
 	
 	@Autowired
 	FtpClientConstant ftpClientConstant;
-	
+
 	@Override
-    public void run(ApplicationArguments args) throws Exception {
-		FtpClientUtils.open(ftpClientConstant);
+	public void run(ApplicationArguments args) {
+		try {
+			// 
+			// FtpClientUtils.open(ftpClientConstant);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
 	}
-	
+
 }
