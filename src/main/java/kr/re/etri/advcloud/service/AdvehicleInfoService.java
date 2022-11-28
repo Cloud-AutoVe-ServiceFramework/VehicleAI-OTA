3adbcda7d7328364cf5536158cae269b98f2238e
kboksung <kboksung@gmail.com>
2022-11-28 18:37:58 +0900

    update AdvehicleInfoService

--- src/main/java/kr/re/etri/advcloud/service/AdvehicleInfoService.java
+++ src/main/java/kr/re/etri/advcloud/service/AdvehicleInfoService.java
@@ -2,37 +2,67 @@ package kr.re.etri.advcloud.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.re.etri.advcloud.mapper.AdvehicleInfoMapper;
 import kr.re.etri.advcloud.model.AdvehicleInfoVO;
 
+
 @Service
 public class AdvehicleInfoService {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(AdvehicleInfoService.class);
+	
 	@Autowired
 	AdvehicleInfoMapper advehicleInfoMapper;
 
-	public List<AdvehicleInfoVO> selectList(AdvehicleInfoVO param) throws Exception {
-		return advehicleInfoMapper.selectList(param);
+	public List<AdvehicleInfoVO> selectList(AdvehicleInfoVO param) {
+		try {
+			return advehicleInfoMapper.selectList(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 
-	public AdvehicleInfoVO select(String advehicle_id) throws Exception {
-		return advehicleInfoMapper.select(advehicle_id);
+	public AdvehicleInfoVO select(String advehicle_id) {
+		try {
+			return advehicleInfoMapper.select(advehicle_id);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 
-	public int insert(AdvehicleInfoVO param) throws Exception {
-		return advehicleInfoMapper.insert(param);
+	public int insert(AdvehicleInfoVO param) {
+		try {
+			return advehicleInfoMapper.insert(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 
-	public int update(String advehicle_id, AdvehicleInfoVO param) throws Exception {
-		param.setAdvehicle_id(advehicle_id);
-		return advehicleInfoMapper.update(param);
+	public int update(String advehicle_id, AdvehicleInfoVO param) {
+		try {
+			param.setAdvehicle_id(advehicle_id);
+			return advehicleInfoMapper.update(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 
-	public int delete(String advehicle_id) throws Exception {
-		return advehicleInfoMapper.delete(advehicle_id);
+	public int delete(String advehicle_id) {
+		try {
+			return advehicleInfoMapper.delete(advehicle_id);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 
 }
