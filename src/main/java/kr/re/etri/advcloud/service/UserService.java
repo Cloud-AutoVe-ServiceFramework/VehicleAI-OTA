08717b3ef8ce3677148f9e662f29f2f43dd60fe7
jeow7 <49180544+jeow7@users.noreply.github.com>
2022-12-06 18:16:40 +0900

    Update a UserService.java

--- src/main/java/kr/re/etri/advcloud/service/UserService.java
+++ src/main/java/kr/re/etri/advcloud/service/UserService.java
@@ -1,5 +1,7 @@
 package kr.re.etri.advcloud.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,38 +11,70 @@ import kr.re.etri.advcloud.model.UserInfoVO;
 @Service
 public class UserService {
 	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserMapper userMapper;
 	
-	public UserInfoVO select(String id) throws Exception {
-		UserInfoVO param = new UserInfoVO();
-		param.setId(id);
-		
-		return select(param);
+	public UserInfoVO select(String id) {
+		try {
+			UserInfoVO param = new UserInfoVO();
+			param.setId(id);
+			
+			return select(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
-	public UserInfoVO select(String id, String name) throws Exception {
-		UserInfoVO param = new UserInfoVO();
-		param.setId(id);
-		param.setName(name);
-		
-		return select(param);
+	public UserInfoVO select(String id, String name)  {
+		try { 
+			UserInfoVO param = new UserInfoVO();
+			param.setId(id);
+			param.setName(name);
+			
+			return select(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
-	public UserInfoVO select(UserInfoVO param) throws Exception {
-		return userMapper.select(param);
+	public UserInfoVO select(UserInfoVO param) {
+		try {
+			return userMapper.select(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
-	public int insert(UserInfoVO param) throws Exception {
-		return userMapper.insert(param);
+	public int insert(UserInfoVO param) {
+		try {
+			return userMapper.insert(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
-	public int updatePassword(UserInfoVO param) throws Exception {
-		return userMapper.updatePassword(param);
+	public int updatePassword(UserInfoVO param) {
+		try {
+			return userMapper.updatePassword(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
-	public int updateManagedCount(UserInfoVO param) throws Exception {
-		return userMapper.updateManagedCount(param);
+	public int updateManagedCount(UserInfoVO param) {
+		try {
+			return userMapper.updateManagedCount(param);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		}
 	}
 	
 }
