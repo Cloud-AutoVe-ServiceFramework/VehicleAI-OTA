c3dde50d123cc8713bdd0d3a33d166d5e17653d6
jeow7 <49180544+jeow7@users.noreply.github.com>
2022-12-06 18:36:50 +0900

    Update a HmacUtils.java

--- src/main/java/kr/re/etri/advcloud/common/util/HmacUtils.java
+++ src/main/java/kr/re/etri/advcloud/common/util/HmacUtils.java
@@ -6,20 +6,36 @@
 package kr.re.etri.advcloud.common.util;
 
 import org.apache.commons.net.util.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class HmacUtils {
 
+	private static final Logger logger = LoggerFactory.getLogger(HmacUtils.class);
+
 	private static String algorithm = "HmacSHA256";
 
-	public static String Hmac(String key, String message) throws Exception {
-		Mac hasher = Mac.getInstance(algorithm);
-		hasher.init(new SecretKeySpec(key.getBytes("utf-8"), algorithm));
-		byte[] hash = hasher.doFinal(message.getBytes());
+	public static String Hmac(String key, String message)
+			throws NoSuchAlgorithmException, InvalidKeyException {
+		try {
+			Mac hasher = Mac.getInstance(algorithm);
+			hasher.init(new SecretKeySpec(key.getBytes("utf-8"), algorithm));
+			byte[] hash = hasher.doFinal(message.getBytes());
 
-		return Base64.encodeBase64String(hash);
+			return Base64.encodeBase64String(hash);
+		} catch (NoSuchAlgorithmException e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		} catch (InvalidKeyException e) {
+			logger.error(e.getMessage(), e);
+			throw e;
+		} 
 	}
 
 }
